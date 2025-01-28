d288699fad390ba8062d9b767b3372325102fd06
Danielsodera <daniel_sodera@me.com>
2025-01-27 21:45:52 -0500

    Use effect to only call API when search term changes

--- movie-list-app/src/App.jsx
+++ movie-list-app/src/App.jsx
@@ -1,6 +1,7 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import './App.css';
 import SearchBar from './components/SearchBar';
+import SearchResults from './components/SeachResults';
 const tmdbKey = '2add80205a34117439e456a51c8b07f2';
 const tmdbBaseUrl ='https://api.themoviedb.org/3'
 const tmdbReadAccessToken = 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyYWRkODAyMDVhMzQxMTc0MzllNDU2YTUxYzhiMDdmMiIsIm5iZiI6MTczNTc1OTEyMS41NDgsInN1YiI6IjY3NzU5NTExNDExMTU5OWUzODEyZTc0YyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.uRqRrmRzXCuMosrX13p3urdQ_P-Efpnw0nmhOATtZl0'
@@ -10,6 +11,7 @@ function App() {
   
    //Search state
   const [searchTerm, setSearchTerm] = useState("Batman");
+  const [movieImages, setMovieImages] = useState([])
 
     function handleSearch(e){
     setSearchTerm(e.target.value)
@@ -23,11 +25,16 @@ function App() {
         Authorization: `Bearer ${tmdbReadAccessToken}`
       }
     };
-    
-    fetch(`https://api.themoviedb.org/3/search/movie?query=${searchTerm}&include_adult=false&language=en-US&page=1`, options)
+    useEffect(() => {
+      fetch(`https://api.themoviedb.org/3/search/movie?query=${searchTerm}&include_adult=false&language=en-US&page=1`, options)
       .then(res => res.json())
-      .then(res => console.log(res))
+      .then(res => {
+        return setMovieImages(res.results[0].poster_path)})
       .catch(err => console.error(err));
+    }, [searchTerm])
+    
+    console.log(movieImages)
+      //this is happening constantly 
 
 
   return (
@@ -37,6 +44,7 @@ function App() {
       <h2>Add movies to your watchlist and tracked the ones you have watched!</h2>
       </header>
     <SearchBar searchTerm={searchTerm} handleSearch={handleSearch} />
+    <SearchResults image={movieImages}/> 
 
     </>
   )
---
+++ movie-list-app/src/components/SeachResults.jsx
@@ -0,0 +1,15 @@
+import React from 'react'; 
+
+export default function SearchResults({movieImages}) {
+
+
+    return (
+        <div className="search-results-container">
+            <img src={movieImages} />
+
+
+
+
+        </div>
+    )
+}
