02d9585c0c224c8126c75bd3e893d78dd5de3184
Danielsodera <daniel_sodera@me.com>
2025-01-28 08:18:35 -0500

    changed h2 text

--- movie-list-app/src/App.jsx
+++ movie-list-app/src/App.jsx
@@ -47,7 +47,7 @@ function App() {
     <>
     <header className="header-container">
       <h1>Movie List</h1>
-      <h2>Add movies to your watchlist and tracked the ones you have watched!</h2>
+      <h2>Add movies to your watchlist and tracked the ones you have seen!</h2>
       </header>
 
       <div className="search-bar-container">
